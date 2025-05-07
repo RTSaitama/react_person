e28cdf890f86c4cccbd7d2e67be4b4065eec182e
RTSaitama <rtsaitama@outlook.com>
2025-05-07 21:51:23 +0300

    solution added with no list rendering for persons

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ name, age = 100, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+
+    {age !== undefined && <p className="Person__age">I am {age}</p>}
+
+    {partnerName ? (
+      <p className="Person__partner">{partnerName} is my wife</p>
+    ) : (
+      <p className="Person__partner">I am free now</p>
+    )}
+  </section>
+);
