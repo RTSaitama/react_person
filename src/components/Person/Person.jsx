b4b2206a1a2e975c99f73ee94ec14e905fac90a2
RTSaitama <rtsaitama@outlook.com>
2025-05-07 22:01:18 +0300

    added conditional rendering for m and f sex

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,13 @@
-export const Person = ({ name, age = 100, partnerName }) => (
+export const Person = ({ name, age = 100, sex, partnerName }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {name}</h2>
 
-    {age !== undefined && <p className="Person__age">I am {age}</p>}
+    <p className="Person__age">I am {age}</p>
 
     {partnerName ? (
-      <p className="Person__partner">{partnerName} is my wife</p>
+      <p className="Person__partner">
+        {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+      </p>
     ) : (
       <p className="Person__partner">I am free now</p>
     )}
